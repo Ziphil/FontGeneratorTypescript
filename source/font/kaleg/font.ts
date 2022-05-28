c82f6e478b5f0ba6deb8dee3ee15b5ef4509f67e
Ziphil <yfe23457@nifty.com>
2022-05-28 21:29:21 +0900

    Kalegフォントのバージョン番号を変更

--- source/font/kaleg/font.ts
+++ source/font/kaleg/font.ts
@@ -49,7 +49,7 @@ export class KalegFont extends Font<KalegGenerator> {
   }
 
   protected createInfo(): FontInfo {
-    let info = new FontInfo("Ziphil", "0.0.0");
+    let info = new FontInfo("Ziphil", "1.0.0");
     return info;
   }
 
