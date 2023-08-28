4a79025037b0301fc5552059cb43b7ae95a1e3aa
Ziphil <yfe23457@nifty.com>
2023-08-28 19:56:30 +0900

    Colfomフォントにグリフを追加

--- source/font/colfom/generator.ts
+++ source/font/colfom/generator.ts
@@ -8,7 +8,8 @@ import {
   Metrics,
   Part,
   generator,
-  glyph
+  glyph,
+  part
 } from "../../module";
 
 
@@ -31,6 +32,10 @@ export class ColfomGenerator extends Generator<ColfomConfig> {
     return 500;
   }
 
+  private get overshoot(): number {
+    return 15;
+  }
+
   private get extraDescent(): number {
     return 25;
   }
@@ -39,16 +44,28 @@ export class ColfomGenerator extends Generator<ColfomConfig> {
     return 25;
   }
 
+  private get virtualDescent(): number {
+    return this.descent - this.overshoot;
+  }
+
+  private get virtualMean(): number {
+    return this.mean + 2 * this.overshoot - this.thickness;
+  }
+
   private get bearing(): number {
     return this.bowlWidth * 0.09;
   }
 
   private get thickness(): number {
-    return this.config.weightConst * 100;
+    return this.config.weightConst * 90;
+  }
+
+  private get bowlHeight(): number {
+    return this.virtualMean;
   }
 
   private get bowlWidth(): number {
-    return this.mean * 1;
+    return this.virtualMean * 0.9;
   }
 
   private createBearings(): Bearings {
@@ -58,12 +75,158 @@ export class ColfomGenerator extends Generator<ColfomConfig> {
     return bearings;
   }
 
-  @glyph("#")
-  public glyphTest(): Glyph {
+  @part()
+  public partBowl(): Part {
     const part = Part.union(
-      Part.circle($(this.bowlWidth / 2, -this.bowlWidth / 2), this.bowlWidth / 2).toStroke(this.thickness / 2, "round", "round")
+      Part.circle($.origin, this.bowlHeight / 2).scale(this.bowlWidth / this.bowlHeight, 1).toStroke(this.thickness / 2, "round", "round")
     );
+    return part;
+  }
+
+  private get tailDepth(): number {
+    return this.bowlHeight / 2 * 0.5;
+  }
+
+  @part()
+  public partLesTail(): Part {
+    const radius = this.bowlHeight / 2;
+    const startPoint = $(Math.sqrt(this.tailDepth * (radius * 2 - this.tailDepth)), radius - this.tailDepth);
+    const endPoint = startPoint.add($(-(this.tailDepth + this.virtualDescent) * (radius - this.tailDepth) / Math.sqrt(this.tailDepth * (radius * 2 - this.tailDepth)), this.tailDepth + this.virtualDescent));
+    const part = Part.union(
+      Part.line(startPoint, endPoint).scale(this.bowlWidth / this.bowlHeight, 1).toStroke(this.thickness / 2, "round", "round")
+    );
+    return part;
+  }
+
+  private get transphoneGap(): number {
+    return this.bowlWidth * 0.16;
+  }
+
+  private get transphoneMargin(): number {
+    return this.mean * 0.03;
+  }
+
+  @part()
+  public partTransphone(): Part {
+    const startPoint = $(0, -this.bowlHeight / 2 + this.transphoneMargin);
+    const endPoint = $(0, this.bowlHeight / 2 - this.transphoneMargin);
+    const part = Part.union(
+      Part.line(startPoint, endPoint).toStroke(this.thickness / 2, "round", "round")
+    );
+    return part;
+  }
+
+  @glyph("a", "A")
+  public glyphAt(): Glyph {
+    const part = Part.union(
+      this.partBowl()
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
     const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("l", "L")
+  public glyphLes(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail()
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("r", "R")
+  public glyphRes(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail(),
+      this.partTransphone().translate($(this.bowlWidth / 2 + this.thickness + this.transphoneGap, 0))
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("p", "P")
+  public glyphPal(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail().rotateHalfTurn()
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("b", "B")
+  public glyphBol(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail().rotateHalfTurn(),
+      this.partTransphone().translate($(this.bowlWidth / 2 + this.thickness + this.transphoneGap, 0))
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("c", "C")
+  public glyphCal(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail().reflectHor()
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("q", "Q")
+  public glyphQol(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail().reflectHor(),
+      this.partTransphone().translate($(this.bowlWidth / 2 + this.thickness + this.transphoneGap, 0))
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("k", "K")
+  public glyphKal(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail().reflectVer()
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  @glyph("g", "G")
+  public glyphGol(): Glyph {
+    const part = Part.union(
+      this.partBowl(),
+      this.partLesTail().reflectVer(),
+      this.partTransphone().translate($(this.bowlWidth / 2 + this.thickness + this.transphoneGap, 0))
+    );
+    part.translate($(this.bowlWidth / 2 + this.thickness / 2, -this.mean / 2));
+    const glyph = Glyph.byBearings(part, this.createBearings());
+    return glyph;
+  }
+
+  private get spaceWidth(): number {
+    return this.bowlWidth * 0.6;
+  }
+
+  @glyph(" ")
+  public glyphSpace(): Glyph {
+    const part = Part.empty();
+    const bearings = {left: this.spaceWidth, right: 0};
+    const glyph = Glyph.byBearings(part, bearings);
     return glyph;
   }
 
