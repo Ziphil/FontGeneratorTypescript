79ba3fa7476b89e515cfb4eefb396bdfc085fbfa
Ziphil <yfe23457@nifty.com>
2023-08-29 20:43:49 +0900

    リファクタリング

--- source/font/vekos/font.ts
+++ source/font/vekos/font.ts
@@ -16,20 +16,20 @@ export class VekosFont extends Font<VekosGenerator> {
 
   private weight: FontWeight;
   private stretch: FontStretch;
-  private high: boolean;
+  private special?: "high";
 
-  public constructor(weight: FontWeight, stretch: FontStretch, high: boolean) {
+  public constructor(weight: FontWeight, stretch: FontStretch, special?: "high") {
     super();
     this.weight = weight;
     this.stretch = stretch;
-    this.high = high;
+    this.special = special;
     this.setup();
   }
 
   protected createFamilyName(): string {
     let familyName = "Vekos";
-    if (this.high) {
-      familyName += " High";
+    if (this.special !== undefined) {
+      familyName += " " + this.special.charAt(0).toUpperCase() + this.special.slice(1);
     }
     return familyName;
   }
@@ -50,7 +50,7 @@ export class VekosFont extends Font<VekosGenerator> {
     const stretchNumber = style.getStretchNumber();
     const weightConst = (weightNumber * 0.5 + 100) / 300;
     const stretchConst = (stretchNumber < 100) ? (stretchNumber * 0.6 + 40) / 100 : stretchNumber / 100;
-    const contrastRatio = (this.high) ? 0.2 : 0.75;
+    const contrastRatio = (this.special === "high") ? 0.2 : 0.75;
     const config = {weightConst, stretchConst, contrastRatio};
     const generator = new VekosGenerator(config);
     return generator;
